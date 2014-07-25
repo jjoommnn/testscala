36ba4918c1c4683f1fc393e64794ce527b466c87
jjoommnn <jjoommnn@gmail.com>
2014-07-25 17:10:05 +0900

    .

--- src/jjoommnn/scala/test/HelloApp6.scala
+++ src/jjoommnn/scala/test/HelloApp6.scala
@@ -7,4 +7,11 @@ object HelloApp6 extends App
     val r = for( i <- list ) yield { println( i ); i + 1 }
     
     println( r );
+    
+    class FuncClass( x:Int ) {
+        def apply( y:Int ) = println( x + y );
+    }
+    
+    val fc = new FuncClass( 10 );
+    fc( 20 );
 }
